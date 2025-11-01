a3be04add60677cda5842656b27041162f1da4e1
Parfait KBKL <parfaitkubikulave@gmail.com>
2025-11-01 16:00:56 +0100

    update counter

--- assets/js/main.js
+++ assets/js/main.js
@@ -312,25 +312,43 @@ jQuery(function ($) {
 	});
 	
 	// Count Time 
-	function makeTimer() {
-		var endTime = new Date("november 01, 2025 17:00:00 PDT");
-		var endTime = (Date.parse(endTime)) / 1000;
-		var now = new Date();
-		var now = (Date.parse(now) / 1000);
-		var timeLeft = endTime - now;
-		var days = Math.floor(timeLeft / 86400); 
-		var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
-		var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
-		var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
-		if (hours < "10") { hours = "0" + hours; }
-		if (minutes < "10") { minutes = "0" + minutes; }
-		if (seconds < "10") { seconds = "0" + seconds; }
-		$("#days").html(days + "<span>Jours</span>");
-		$("#hour").html(hours + "<span>Heures</span>");
-		$("#minu").html(minutes + "<span>Minutes</span>");
-		$("#seco").html(seconds + "<span>Secondes</span>");
-	}
-	setInterval(function() { makeTimer(); }, 300);
+    function makeTimer() {
+        // Heure du Congo (UTC+1) pour le 1er novembre 2025 à 00:00
+        var endTime = new Date("November 01, 2025 00:00:00 GMT+0100");
+        endTime = Date.parse(endTime) / 1000;
+
+        var now = new Date();
+        now = Date.parse(now) / 1000;
+
+        var timeLeft = endTime - now;
+
+        // Si l'événement est déjà passé
+        if (timeLeft <= 0) {
+            $("#days").html("0<span>Jours</span>");
+            $("#hour").html("0<span>Heures</span>");
+            $("#minu").html("0<span>Minutes</span>");
+            $("#seco").html("0<span>Secondes</span>");
+            return; // stoppe le reste
+        }
+
+        var days = Math.floor(timeLeft / 86400); 
+        var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
+        var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600)) / 60);
+        var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
+
+        if (hours < 10) { hours = "0" + hours; }
+        if (minutes < 10) { minutes = "0" + minutes; }
+        if (seconds < 10) { seconds = "0" + seconds; }
+
+        $("#days").html(days + "<span>Jours</span>");
+        $("#hour").html(hours + "<span>Heures</span>");
+        $("#minu").html(minutes + "<span>Minutes</span>");
+        $("#seco").html(seconds + "<span>Secondes</span>");
+    }   
+
+    // Mise à jour toutes les 0.3s
+    setInterval(function() { makeTimer(); }, 300);
+
     
 	// WOW JS
 	$(window).on('load', function () {
