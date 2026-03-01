3a4d4955bd46e385f3fbedee96dbc8b953874819
JeremieKorangi <jerekora@gmail.com>
2026-03-01 22:57:28 +0100

    ultimi

--- config.php
+++ config.php
@@ -1,11 +1,12 @@
 <?php
+
 // config.php
 session_start();
 
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'fickin_db');
+define('DB_HOST', 'db5019915564.hosting-data.io');
+define('DB_USER', 'dbu1371380');
+define('DB_PASS', '9KLbER3NNkM@7Xc');
+define('DB_NAME', 'dbs15381844');
 
 function getDB() {
     $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
@@ -18,6 +19,7 @@ function getDB() {
     
     return $conn;
 }
+
 
 function isAdmin() {
     return isset($_SESSION['admin_id']);
